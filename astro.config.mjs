2d1fca141d79b5910cbd72d77ba368d632dd2d3f
Jeremy Jones <jeremy@salish.dev>
2025-07-30 17:09:12 -0700

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
+      enabled: true,
     },
-
-    imageService: "cloudflare"
+    imageService: "compile",
   }),
 });
